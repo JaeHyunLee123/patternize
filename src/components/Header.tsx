aa1c530f624000a3591f93f63ef70044c7f3a2a9
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-12-16 17:34:41 +0900

    feat: implement github login"
    ;
    q
    ;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,25 @@
-const Header = () => {
+import { getAuthSession } from "@/lib/auth";
+import { Button } from "./ui/Button";
+import Link from "next/link";
+
+const Header = async () => {
+  const session = await getAuthSession();
+
   return (
-    <header className="px-10 py-5 font-bold text-xl text-foreground bg-background border-b-2 border-b-border">
-      Patternize
+    <header className="px-10 py-5 text-foreground bg-background border-b-2 border-b-border flex justify-between">
+      <Link href="/">
+        <h1 className="font-bold text-xl">Patternize</h1>
+      </Link>
+
+      {session?.user ? (
+        <Button variant={"destructive"}>
+          <Link href="/sign-out">Sign Out</Link>
+        </Button>
+      ) : (
+        <Button>
+          <Link href="/sign-in">Sign In</Link>
+        </Button>
+      )}
     </header>
   );
 };
