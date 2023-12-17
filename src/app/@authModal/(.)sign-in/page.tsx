8829b4e9c2f87f764a0e1c8bfdf411957882ad8d
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-12-17 13:01:57 +0900

    refactor: seperate Modal component

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -1,29 +1,11 @@
-"use client";
 import SignIn from "@/components/SignIn";
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/Button";
-import { X } from "lucide-react";
-const Page = () => {
-  const router = useRouter();
+import Modal from "@/components/Modal";
 
+const Page = () => {
   return (
-    <div className="fixed inset-0 bg-zinc-900/20 z-10">
-      <div className="container flex items-center h-full mx-w-lg mx-auto">
-        <div className="relative bg-white w-full h-fit py-20 px-2 rounded-lg">
-          <div className="absolute top-4 right-4">
-            <Button
-              onClick={() => router.back()}
-              aria-label="close modal"
-              variant={"secondary"}
-              className="h-6 w-6 p-0 rounded-md"
-            >
-              <X className="h-4 w-4" />
-            </Button>
-          </div>
-          <SignIn />
-        </div>
-      </div>
-    </div>
+    <Modal>
+      <SignIn />
+    </Modal>
   );
 };
 
---
+++ src/components/Modal.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/Button";
+import { X } from "lucide-react";
+
+const Modal = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  return (
+    <div className="fixed inset-0 bg-zinc-900/20 z-10">
+      <div className="container flex items-center h-full mx-w-lg mx-auto">
+        <div className="relative bg-white w-full h-fit py-20 px-2 rounded-lg">
+          <div className="absolute top-4 right-4">
+            <Button
+              onClick={() => router.back()}
+              aria-label="close modal"
+              variant={"secondary"}
+              className="h-6 w-6 p-0 rounded-md"
+            >
+              <X className="h-4 w-4" />
+            </Button>
+          </div>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
