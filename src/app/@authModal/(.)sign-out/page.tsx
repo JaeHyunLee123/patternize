2a2be1f8025e86956b908cfd7a3c29858acaa20f
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-12-17 13:06:45 +0900

    feat: implment signout modal

---
+++ src/app/@authModal/(.)sign-out/page.tsx
@@ -0,0 +1,12 @@
+import Modal from "@/components/Modal";
+import SignOut from "@/components/SignOut";
+
+const Page = () => {
+  return (
+    <Modal>
+      <SignOut />
+    </Modal>
+  );
+};
+
+export default Page;
