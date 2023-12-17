1e8b05533918e8c42f914289ecfd08d6588fdedd
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-12-17 13:05:38 +0900

    refactor: seperate SignOut component

--- src/app/sign-out/page.tsx
+++ src/app/sign-out/page.tsx
@@ -1,22 +1,6 @@
-"use client";
-import { signOut } from "next-auth/react";
-import { Button } from "@/components/ui/Button";
-
+import SignOut from "@/components/SignOut";
 const Page = () => {
-  return (
-    <div className="flex flex-col w-full p-24 justify-center items-center gap-4">
-      <span className="text-foreground text-lg">
-        Do you really want to Sign Out?
-      </span>
-      <Button
-        variant={"destructive"}
-        className="w-28"
-        onClick={() => signOut()}
-      >
-        Sign Out
-      </Button>
-    </div>
-  );
+  return <SignOut />;
 };
 
 export default Page;
