a37fb0d5745089d095a6af5ba14b206f1e7647ac
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-12-18 15:58:02 +0900

    feat: implement get daily routine api

---
+++ src/app/api/routine/daily/route.ts
@@ -0,0 +1,47 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { createRoutineControl } from "@/lib/utils";
+
+export const GET = async (req: Request) => {
+  try {
+    const session = await getAuthSession();
+
+    if (!session?.user) return new Response("Unauthorized", { status: 401 });
+
+    const body = await req.json();
+    const { userId } = body;
+
+    if (!userId || typeof userId !== "string")
+      return new Response("Need userid", { status: 400 });
+
+    let routineControl = await db.routineControl.findUnique({
+      where: {
+        userId,
+      },
+      include: {
+        dailyRoutines: true,
+      },
+    });
+
+    if (!routineControl) {
+      createRoutineControl(userId);
+      routineControl = await db.routineControl.findUnique({
+        where: {
+          userId,
+        },
+        include: {
+          dailyRoutines: true,
+        },
+      });
+    }
+
+    if (!routineControl)
+      return new Response("Cannot find routine", { status: 500 });
+
+    const dailyRoutines = routineControl.dailyRoutines;
+
+    return Response.json(dailyRoutines, { status: 201 });
+  } catch (error) {
+    return new Response("Unknown Error", { status: 500 });
+  }
+};
