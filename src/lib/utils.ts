76598fb9142d87e983d61439dabd9b29dd20faf5
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-12-18 15:31:06 +0900

    feat: implement createRoutineControl util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { db } from "./db";
+import { Prisma } from "@prisma/client";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const createRoutineControl = async (userid: string) => {
+  try {
+    await db.routineControl.create({
+      data: {
+        userId: userid,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        console.log("This user already has routineControl");
+      }
+    }
+  }
+};
