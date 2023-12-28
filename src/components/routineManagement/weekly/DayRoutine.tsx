d25aad0a3e03a5a77b148077f7554f88ed784a72
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-12-28 12:36:52 +0900

    feat: implement frontend for read weekly routine

---
+++ src/components/routineManagement/weekly/DayRoutine.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+
+import type { Day, WeeklyRoutine } from "@prisma/client";
+
+interface DayRoutineProps {
+  routines?: WeeklyRoutine[] | null;
+  day: Day;
+}
+
+const DayRoutine: FC<DayRoutineProps> = ({ routines, day }) => {
+  const thisDayRoutines = routines?.filter((routine) => {
+    routine.day === day;
+  });
+
+  return (
+    <div>
+      <p className="text-lg font-medium">{day}</p>
+      {!thisDayRoutines || thisDayRoutines.length === 0 ? (
+        <p>No routine</p>
+      ) : (
+        thisDayRoutines.map((routine) => (
+          <p key={routine.id}>{routine.content}</p>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default DayRoutine;
--- src/components/routineManagement/weekly/ShowWeeklyRoutineCard.tsx
+++ src/components/routineManagement/weekly/ShowWeeklyRoutineCard.tsx
@@ -2,14 +2,49 @@
 
 import { FC } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import type { GetWeeklyRoutinesAPI } from "@/types/API";
+import { Separator } from "@/components/ui/Separator";
+import DayRoutine from "./DayRoutine";
 
 const ShowWeeklyRoutineCard: FC = () => {
+  const { data, isPending, refetch } = useQuery<GetWeeklyRoutinesAPI>({
+    queryKey: ["weekly routines"],
+    queryFn: async () => {
+      const response = await axios.get("/api/routine/weekly");
+
+      return response.data;
+    },
+  });
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Weekly Routines</CardTitle>
       </CardHeader>
-      <CardContent>Show weekly routine here</CardContent>
+      <CardContent>
+        {isPending ? (
+          <p>Loading data...</p>
+        ) : (
+          <div className="flex flex-col">
+            <DayRoutine routines={data?.weeklyRoutines} day="SUN" />
+            <Separator />
+            <DayRoutine routines={data?.weeklyRoutines} day="MON" />
+            <Separator />
+            <DayRoutine routines={data?.weeklyRoutines} day="TUE" />
+            <Separator />
+            <DayRoutine routines={data?.weeklyRoutines} day="WED" />
+            <Separator />
+            <DayRoutine routines={data?.weeklyRoutines} day="THU" />
+            <Separator />
+            <DayRoutine routines={data?.weeklyRoutines} day="FRI" />
+            <Separator />
+            <DayRoutine routines={data?.weeklyRoutines} day="SAT" />
+            <Separator />
+          </div>
+        )}
+      </CardContent>
     </Card>
   );
 };
