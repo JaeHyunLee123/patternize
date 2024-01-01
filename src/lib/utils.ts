d59478e25aa7477fe0de8ee653c70a9b51f5f0c3
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-01-01 17:00:17 +0900

    feat: implement isDayMatch function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,29 @@ import { twMerge } from "tailwind-merge";
 import { db } from "./db";
 import { Prisma } from "@prisma/client";
 
+import type { Days, Day } from "@prisma/client";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const isDayMatch = ({ days, day }: { days: Days; day: Day }) => {
+  switch (day) {
+    case "SUN":
+      return days.sun;
+    case "MON":
+      return days.mon;
+    case "TUE":
+      return days.tue;
+    case "WED":
+      return days.wed;
+    case "THU":
+      return days.thu;
+    case "FRI":
+      return days.fri;
+    case "SAT":
+      return days.sat;
+    default:
+      return false;
+  }
+};
