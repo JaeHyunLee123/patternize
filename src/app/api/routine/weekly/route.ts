51b4a7b97c3cb50d82a9d9d77d3b620197fe0cf8
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-01-03 16:59:59 +0900

    feat: implement backend for update weekly routine

--- src/app/api/routine/weekly/route.ts
+++ src/app/api/routine/weekly/route.ts
@@ -205,3 +205,95 @@ export const DELETE = async (req: Request) => {
     );
   }
 };
+
+export const PUT = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { routineId, newContent, days } = body;
+
+    if (!routineId || typeof routineId !== "string")
+      return Response.json(
+        { errorMessage: "Need routineId", ok: false },
+        { status: 400 }
+      );
+
+    if (!newContent || typeof newContent !== "string")
+      return Response.json(
+        { errorMessage: "Need content", ok: false },
+        { status: 400 }
+      );
+
+    if (!(1 < newContent.length && newContent.length < 201))
+      return Response.json(
+        { errorMessage: "Invalid content", ok: false },
+        { status: 400 }
+      );
+
+    if (!days || !Array.isArray(days) || days.length === 0) {
+      return Response.json(
+        { errorMessage: "Need days", ok: false },
+        { status: 400 }
+      );
+    }
+
+    const session = await getAuthSession();
+
+    if (!session?.user)
+      return Response.json(
+        { errorMessage: "Unauthorized", ok: false },
+        { status: 401 }
+      );
+
+    const routine = await db.weeklyRoutine.findUnique({
+      where: {
+        id: routineId,
+      },
+      include: {
+        routineControl: true,
+      },
+    });
+
+    if (!routine || !routine.routineControlId)
+      return Response.json(
+        { errorMessage: "Cannot find routine", ok: false },
+        { status: 500 }
+      );
+
+    if (session.user.id !== routine.routineControl?.userId)
+      return Response.json(
+        { errorMessage: "Unauthorized", ok: false },
+        { status: 401 }
+      );
+
+    await db.days.update({
+      where: {
+        id: routine.daysId,
+      },
+      data: {
+        sun: days.includes("sun"),
+        mon: days.includes("mon"),
+        tue: days.includes("tue"),
+        wed: days.includes("wed"),
+        thu: days.includes("thu"),
+        fri: days.includes("fri"),
+        sat: days.includes("sat"),
+      },
+    });
+
+    await db.weeklyRoutine.update({
+      where: {
+        id: routineId,
+      },
+      data: {
+        content: newContent,
+      },
+    });
+
+    return Response.json({ ok: true }, { status: 201 });
+  } catch (error) {
+    return Response.json(
+      { errorMessage: "Unknown Error", ok: false },
+      { status: 500 }
+    );
+  }
+};
