38706b551d8e7ebb4ab156977315bd818549e257
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-01-12 02:16:48 +0900

    feat: implement backend for delete monthly routine

--- src/app/api/routine/monthly/route.ts
+++ src/app/api/routine/monthly/route.ts
@@ -261,3 +261,76 @@ export const POST = async (req: Request) => {
     );
   }
 };
+
+export const PUT = async (req: Request) => {};
+
+export const DELETE = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { routineId } = body;
+
+    if (!routineId || typeof routineId !== "string")
+      return Response.json(
+        { errorMessage: "Need routineId", ok: false },
+        { status: 400 }
+      );
+
+    const session = await getAuthSession();
+
+    if (!session?.user)
+      return Response.json(
+        { errorMessage: "Unauthorized", ok: false },
+        { status: 401 }
+      );
+
+    const routine = await db.monthlyRoutine.findUnique({
+      where: {
+        id: routineId,
+      },
+      include: {
+        routineControl: true,
+      },
+    });
+
+    if (!routine || !routine.routineControlId)
+      return Response.json(
+        { errorMessage: "Cannot find routine", ok: false },
+        { status: 500 }
+      );
+
+    if (session.user.id !== routine.routineControl?.userId)
+      return Response.json(
+        { errorMessage: "Unauthorized", ok: false },
+        { status: 401 }
+      );
+
+    await db.monthlyRoutine.delete({
+      where: {
+        id: routineId,
+      },
+    });
+
+    if (routine.dateId) {
+      await db.date.delete({
+        where: {
+          id: routine.dateId,
+        },
+      });
+    }
+
+    if (routine.weekAndDayId) {
+      await db.weekAndDay.delete({
+        where: {
+          id: routine.weekAndDayId,
+        },
+      });
+    }
+
+    return Response.json({ ok: true }, { status: 201 });
+  } catch (error) {
+    return Response.json(
+      { errorMessage: "Unknown Error", ok: false },
+      { status: 500 }
+    );
+  }
+};
