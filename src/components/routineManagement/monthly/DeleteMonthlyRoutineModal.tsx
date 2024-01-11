b9aac4649c8544e326e53873d8debc249be0b2a8
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-01-12 02:09:56 +0900

    feat: implement frontend for delete monthly routine

--- src/components/routineManagement/monthly/DeleteMonthlyRoutineModal.tsx
+++ src/components/routineManagement/monthly/DeleteMonthlyRoutineModal.tsx
@@ -13,7 +13,13 @@ import {
 } from "@/components/ui/AlertDialog";
 import { Button, buttonVariants } from "@/components/ui/Button";
 import { SheetClose } from "@/components/ui/Sheet";
-import { RefetchOptions, QueryObserverResult } from "@tanstack/react-query";
+import { useToast } from "@/hook/use-toast";
+import {
+  RefetchOptions,
+  QueryObserverResult,
+  useMutation,
+} from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
 import { FC } from "react";
 
 interface DeleteMonthlyRoutineModalProps {
@@ -23,7 +29,35 @@ interface DeleteMonthlyRoutineModalProps {
   ) => Promise<QueryObserverResult<unknown, Error>>;
 }
 
-const DeleteMonthlyRoutineModal: FC<DeleteMonthlyRoutineModalProps> = ({}) => {
+const DeleteMonthlyRoutineModal: FC<DeleteMonthlyRoutineModalProps> = ({
+  routineId,
+  refetch,
+}) => {
+  const { toast } = useToast();
+
+  const { mutate } = useMutation({
+    mutationFn: () => {
+      const payload = { routineId };
+      return axios.delete("/api/routine/monthly", { data: payload });
+    },
+    onSuccess: () => {
+      refetch();
+      toast({
+        title: "Delete Monthly Routine!",
+        className: "bg-red-300",
+      });
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        toast({
+          title: "Delete Monthly Routine Failed!",
+          description: String(error.response?.data.errorMessage),
+          variant: "destructive",
+        });
+      }
+    },
+  });
+
   return (
     <AlertDialog>
       <AlertDialogTrigger>
@@ -42,6 +76,9 @@ const DeleteMonthlyRoutineModal: FC<DeleteMonthlyRoutineModalProps> = ({}) => {
           <SheetClose asChild>
             <AlertDialogAction
               className={buttonVariants({ variant: "destructive" })}
+              onClick={() => {
+                mutate();
+              }}
             >
               Delete
             </AlertDialogAction>
