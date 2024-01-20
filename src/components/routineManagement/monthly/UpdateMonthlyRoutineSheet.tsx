b7cf04193ba5098a1909d366c04bb4156ff2a4dd
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-01-20 18:10:06 +0900

    feat: implement frontend UpdateMonthlyRoutine, TODO: add default value

--- src/components/routineManagement/monthly/UpdateMonthlyRoutineSheet.tsx
+++ src/components/routineManagement/monthly/UpdateMonthlyRoutineSheet.tsx
@@ -3,6 +3,7 @@
 import { Separator } from "@/components/ui/Separator";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -14,9 +15,22 @@ import {
   MonthlyRoutineWithDate,
   MonthlyRoutineWithWeekAndDay,
 } from "@/types/API";
-import { QueryObserverResult, RefetchOptions } from "@tanstack/react-query";
-import { FC } from "react";
+import {
+  QueryObserverResult,
+  RefetchOptions,
+  useMutation,
+} from "@tanstack/react-query";
+import { FC, useState } from "react";
 import DeleteMonthlyRoutineModal from "./DeleteMonthlyRoutineModal";
+import { useToast } from "@/hook/use-toast";
+import { useForm } from "react-hook-form";
+import axios, { AxiosError } from "axios";
+import { Label } from "@/components/ui/Label";
+import { Input } from "@/components/ui/Input";
+import { Button } from "@/components/ui/Button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/ToggleGroup";
+import OnlyDatePicker from "@/components/OnlyDatePicker";
 
 interface UpdateMonthlyRoutineSheetProps {
   routine: MonthlyRoutineWithDate | MonthlyRoutineWithWeekAndDay;
@@ -26,11 +40,80 @@ interface UpdateMonthlyRoutineSheetProps {
   classname?: string;
 }
 
+interface UpdateMonthlyRoutineForm {
+  isDate: boolean;
+  content: string;
+
+  week?: number;
+  day?: string;
+
+  date?: number;
+}
+
+type monthlyRoutineType = "weekAndDay" | "date";
+
 const UpdateMonthlyRoutineSheet: FC<UpdateMonthlyRoutineSheetProps> = ({
   refetch,
   routine,
   classname,
 }) => {
+  const { toast } = useToast();
+  const [routineType, setRoutineType] =
+    useState<monthlyRoutineType>("weekAndDay");
+
+  const [date, setDate] = useState("");
+  const [day, setDay] = useState("");
+  const [week, setWeek] = useState("");
+
+  const {
+    register,
+    formState: { errors },
+    reset,
+    handleSubmit,
+  } = useForm<UpdateMonthlyRoutineForm>({ mode: "onChange" });
+
+  const { mutate } = useMutation({
+    mutationFn: (form: UpdateMonthlyRoutineForm) => {
+      return axios.put("api/routine/monthly", form);
+    },
+    onSuccess: () => {
+      reset();
+      refetch();
+      toast({
+        title: "Create Weekly Routine Success!",
+        className: "bg-green-300",
+      });
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        toast({
+          title: "Create Weekly Routine Failed!",
+          description: String(error.response?.data.errorMessage),
+          variant: "destructive",
+        });
+      }
+    },
+  });
+
+  const onSubmit = (form: UpdateMonthlyRoutineForm) => {
+    if (routineType === "weekAndDay") {
+      form.week = Number(week);
+      form.day = day.toUpperCase();
+      form.isDate = false;
+
+      if (!(1 <= form.week && form.week <= 4)) return;
+
+      mutate(form);
+    } else {
+      form.date = Number(date);
+      form.isDate = true;
+
+      if (!(1 <= form.date || form.date <= 31)) return;
+
+      mutate(form);
+    }
+  };
+
   return (
     <Sheet>
       <SheetTrigger
@@ -48,8 +131,105 @@ const UpdateMonthlyRoutineSheet: FC<UpdateMonthlyRoutineSheetProps> = ({
             You can update or delete routine here
           </SheetDescription>
         </SheetHeader>
-        <div>TODO: make form for update monthly routine</div>
-        <Separator />
+        <form
+          className="flex flex-col space-y-2 items-center"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Tabs
+            className="w-full"
+            onValueChange={(type) => {
+              if (type === "weekAndDay" || type === "date")
+                setRoutineType(type);
+            }}
+            defaultValue={routine.isDate ? "date" : "weekAndDay"}
+          >
+            <TabsList className="mb-3 w-full">
+              <TabsTrigger className="w-[50%]" value="weekAndDay">
+                Week And Day
+              </TabsTrigger>
+              <TabsTrigger className="w-[50%]" value="date">
+                Date
+              </TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="weekAndDay">
+              <Label htmlFor="week">Select a week</Label>
+              <ToggleGroup
+                type="single"
+                value={week}
+                onValueChange={(week) => {
+                  setWeek(week);
+                }}
+              >
+                <ToggleGroupItem value="1">1st</ToggleGroupItem>
+                <ToggleGroupItem value="2">2nd</ToggleGroupItem>
+                <ToggleGroupItem value="3">3rd</ToggleGroupItem>
+                <ToggleGroupItem value="4">4th</ToggleGroupItem>
+              </ToggleGroup>
+              <Label htmlFor="day">Select a day</Label>
+              <ToggleGroup
+                type="single"
+                value={day}
+                onValueChange={(day) => {
+                  setDay(day);
+                }}
+              >
+                <ToggleGroupItem value="sun">Sun</ToggleGroupItem>
+                <ToggleGroupItem value="mon">Mon</ToggleGroupItem>
+                <ToggleGroupItem value="tue">Tue</ToggleGroupItem>
+                <ToggleGroupItem value="wed">Wed</ToggleGroupItem>
+                <ToggleGroupItem value="thu">Thu</ToggleGroupItem>
+                <ToggleGroupItem value="fri">Fri</ToggleGroupItem>
+                <ToggleGroupItem value="sat">Sat</ToggleGroupItem>
+              </ToggleGroup>
+            </TabsContent>
+            <TabsContent
+              value="date"
+              className="flex items-center justify-center"
+            >
+              <div className="flex flex-col items-center space-y-2">
+                <OnlyDatePicker classname="mt-2" onChange={setDate} />
+                <span className="text-xs text-gray-500">
+                  {date > "28"
+                    ? "This routine will be shown at 28th on February"
+                    : ""}
+                </span>
+                <span className="text-xs text-gray-500">
+                  {date === "31"
+                    ? "This routine will be shown at 30th on April, June , September and November"
+                    : ""}
+                </span>
+              </div>
+            </TabsContent>
+          </Tabs>
+          <div className="w-full">
+            <Label htmlFor="routine">Routine</Label>
+            <Input
+              id="routine"
+              {...register("content", {
+                required: "Please write routine.",
+                minLength: {
+                  value: 2,
+                  message: "Please write more than 2 letters",
+                },
+                maxLength: {
+                  value: 200,
+                  message: "Please write less than 200 letters",
+                },
+              })}
+              defaultValue={routine.content}
+            />
+            <span className="text-red-500">
+              {errors.content ? errors.content.message : ""}
+            </span>
+          </div>
+          <SheetClose asChild>
+            <Button type="submit" className="w-[50%]" variant={"outline"}>
+              Create
+            </Button>
+          </SheetClose>
+        </form>
+        <Separator className="my-2" />
         <div className="flex items-center space-x-2 justify-between">
           <span className="text-foreground text-lg">Delete Routine</span>
           <DeleteMonthlyRoutineModal refetch={refetch} routineId={routine.id} />
